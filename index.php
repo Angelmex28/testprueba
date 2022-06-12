02d964ccab6a4da276d3aad072b5dec523d0899c
Juan Acosta <angelmex121@gmail.com>
2022-06-12 11:51:00 -0400

    cambio 6 creo

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
 <body>
     <h1>Hola Mundo</h1>
     <h2>Este es mi primer proyecto en githud</h2>
+    <br>
+    <br>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam reprehenderit, tempore reiciendis eaque molestiae eligendi sequi explicabo corporis, dignissimos, repellendus eveniet voluptate quo. Natus, numquam porro illum alias quo distinctio.</p>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit fugiat ducimus velit commodi, incidunt dignissimos magnam. Soluta, perferendis in nemo, numquam deleniti, mollitia perspiciatis maiores nihil totam rem consectetur? Deserunt.</p>
 </body>
